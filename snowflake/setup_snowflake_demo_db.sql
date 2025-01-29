f835a3c26af800fd4eee459eef4a9955c852d0fe
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-29 21:58:41 +0000

    error is fixed

--- snowflake/setup_snowflake_demo_db.sql
+++ snowflake/setup_snowflake_demo_db.sql
@@ -5,7 +5,7 @@ CREATE OR ALTER WAREHOUSE demo_db
   AUTO_SUSPEND = 300 
   AUTO_RESUME= TRUE;
 CREATE OR ALTER DATABASE demo_db;
-- Database level objects
+-- Database level objects
 CREATE OR ALTER SCHEMA bronze;
 CREATE OR ALTER SCHEMA silver;
 CREATE OR ALTER SCHEMA gold;
@@ -14,6 +14,6 @@ CREATE OR ALTER SCHEMA gold;
 -- Schema level objects
 CREATE OR REPLACE FILE FORMAT bronze.json_format TYPE = 'json';
 CREATE OR ALTER STAGE bronze.raw;
-
+execute immediate 
 
 
