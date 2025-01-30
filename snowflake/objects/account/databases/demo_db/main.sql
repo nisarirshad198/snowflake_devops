8d2d9e07474a6da85cde5bcea4b2e2e6e9e013ed
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-30 10:22:01 +0000

    file path is corrected

--- snowflake/objects/account/databases/demo_db/main.sql
+++ snowflake/objects/account/databases/demo_db/main.sql
@@ -1,6 +1,6 @@
 execute immediate from './create_database_and_schemas.sql';
 execute immediate from './create_role.sql';
 
-execute immediate from './demo_db/schemas/bronze/create_stages.sql';
+execute immediate from './schemas/bronze/create_stages.sql';
 
-execute immediate from './demo_db/schemas/bronze/create_tables.sql';
+execute immediate from './schemas/bronze/create_tables.sql';
