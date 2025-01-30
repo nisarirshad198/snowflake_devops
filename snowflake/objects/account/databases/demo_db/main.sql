b8f8cef4200f0b99a4f71f9a2ec6183fd5f4e679
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-30 12:42:19 +0000

    TEST

--- snowflake/objects/account/databases/demo_db/main.sql
+++ snowflake/objects/account/databases/demo_db/main.sql
@@ -1,9 +1,8 @@
 
-USE ROLE SYSADMIN;
-USE WAREHOUSE COMPUTE_WH;
 execute immediate from './create_database_and_schemas.sql';
+USE ROLE ACCOUNTADMIN;
 execute immediate from './create_role.sql';
-
+USE ROLE SYSADMIN;
 execute immediate from './schemas/bronze/create_stages.sql';
 
 execute immediate from './schemas/bronze/create_tables.sql';
