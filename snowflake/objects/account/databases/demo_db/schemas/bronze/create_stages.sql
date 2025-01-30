4491bac678620ee5a78fdd4b46a10ed01f78e95a
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-30 20:55:48 +0000

    test

--- snowflake/objects/account/databases/demo_db/schemas/bronze/create_stages.sql
+++ snowflake/objects/account/databases/demo_db/schemas/bronze/create_stages.sql
@@ -1,5 +1,5 @@
 use database DEMO_DB;
 use schema bronze;
 -- Schema level objects
-CREATE OR REPLACE FILE FORMAT bronze.json_format TYPE = 'json';
+CREATE OR REPLACE FILE FORMAT json_format TYPE = 'json';
 CREATE OR ALTER STAGE bronze.raw;
