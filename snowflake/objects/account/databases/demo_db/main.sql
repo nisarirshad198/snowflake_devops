a50c6cefd21a3c2c77f0b7253b69b44445e53117
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-30 13:01:57 +0000

    TEST

--- snowflake/objects/account/databases/demo_db/main.sql
+++ snowflake/objects/account/databases/demo_db/main.sql
@@ -3,6 +3,7 @@ execute immediate from './create_database_and_schemas.sql';
 USE ROLE ACCOUNTADMIN;
 execute immediate from './create_role.sql';
 USE ROLE SYSADMIN;
+USE DATABASE DEMO_COMMON;
 execute immediate from './schemas/bronze/create_stages.sql';
-
+USE DATABASE DEMO_COMMON;
 execute immediate from './schemas/bronze/create_tables.sql';
