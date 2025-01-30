9cf8119716921ed6ec7771c348fb18490c901152
nisarirshad198 <146818704+nisarirshad198@users.noreply.github.com>
2025-01-30 14:16:35 +0000

    TEST

--- snowflake/setup_snowflake.sql
+++ snowflake/setup_snowflake.sql
@@ -34,9 +34,7 @@ GRANT READ , WRITE  ON GIT REPOSITORY demo_common.public.demo_repo TO ROLE SYSAD
   WAREHOUSE_SIZE = XSMALL 
   AUTO_SUSPEND = 300 
   AUTO_RESUME= TRUE;
-USE WAREHOUSE COMPUTE_WH;
-USE ROLE ACCOUNTADMIN;
-USE DATABASE DEMO_COMMON;
+
 execute immediate from './objects/account/databases/demo_db/main.sql';
 
 
